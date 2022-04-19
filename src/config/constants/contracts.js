ed728440c694e9fc32ea7c586d3331428001481e
Sha <1473340258@qq.com>
2022-04-20 00:04:40 +0800

    初始化

--- src/config/constants/contracts.js
+++ src/config/constants/contracts.js
@@ -1,4 +1,4 @@
 export default {
-  //market: '0xf4702D65480Df559B21dBaa6bb31b8E52AaD495d',
-  market: '0x09a151fC02f26d04721CC0e2Ec3B40C65C9fC088',
+  market: '0xf4702D65480Df559B21dBaa6bb31b8E52AaD495d', //测试网合约地址
+  //market: '0x09a151fC02f26d04721CC0e2Ec3B40C65C9fC088',
 };
